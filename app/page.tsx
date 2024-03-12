4761cb92695779f30387e1d0eb76e47c01d55a6c
afyqeyzar <norafyqe@gmail.com>
2024-03-12 23:41:19 +0000

    feat: scroll animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+"use client";
 import VideoHero from "@/components/VideoHero/VideoHero";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(ScrollTrigger);
 export default function Home() {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".bio",
+      {
+        y: "100vh",
+      },
+      {
+        y: "100vh",
+        duration: 5,
+        scrollTrigger: {
+          trigger: ".bio-title",
+          toggleActions: "restart none none none",
+          // start: "bottom bottom",
+          // end: "top 20%",
+          scrub: true,
+        },
+      },
+    );
+  });
   return (
-    <main>
-      <section className="h-screen w-screen">
+    <main className="h-[200vh]">
+      <section className="video fixed h-screen w-screen">
         <img
           src="/svg/eyzar-white.svg"
           alt="afyq eyzar"
@@ -11,8 +35,8 @@ export default function Home() {
         />
         <VideoHero />
       </section>
-      <section className="h-screen p-3">
-        <h1 className="text-7xl">here is my bio</h1>
+      <section className="bio z-50 h-screen bg-slate-50 p-3">
+        <h1 className="bio-title text-7xl">here is my bio</h1>
         <a href="#">link to somewhere</a>
       </section>
     </main>
